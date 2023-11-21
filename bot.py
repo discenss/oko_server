515b23b7ff9a17990f11a728a8e737b1898802f0
discens <discenss@gmail.com>
2023-11-21 16:12:44 +0200

    Added scripts

---
+++ bot.py
@@ -0,0 +1,208 @@
+import telebot
+from telebot import types
+from datetime import datetime
+from db import DB
+from telebot.types import LabeledPrice, ShippingOption
+import time
+
+
+def read_bot_keys(file_path):
+    with open(file_path, 'r') as file:
+        first_line = file.readline().rstrip()
+        second_line = file.readline().rstrip()
+
+    return first_line, second_line
+
+# Пример использования функции
+file_path = 'file.txt'  # Укажите путь к вашему файлу
+bot_id, payment_token = read_bot_keys(file_path)
+
+bot = telebot.TeleBot(bot_id)
+
+report_for_date = 'Введіть дату у форматі РРРР-ММ-ДД (Наприклад 1991-08-24)'
+report_for_date_or_exit = "Ведіть дату у форматі РРРР-ММ-ДД (Наприклад 1991-08-24) или /menu для виходу"
+est_name_and_password_for_subsc = "Введіть назву_закладу та пароль через пробіл. Наприклад:\n MyBar MyPassword"
+top_up_account = "Введіть суму поповнення"
+
+def send_bot_message(message, tg_id):
+    bot.send_message(tg_id, message)
+
+@bot.pre_checkout_query_handler(func=lambda query: True)
+def process_pre_checkout(query):
+    bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=True)
+
+@bot.message_handler(content_types=['successful_payment'])
+def handle_successful_payment(message):
+    db = DB()
+    db.addmomey_for_tg_user(message.from_user.id, message.successful_payment.total_amount / 100)
+    bot.reply_to(message, "Вы успешно пополнили счёт. Баланс %s" % db.get_money_for_tg_user(message.from_user.id))
+    show_main_menu(message)
+
+def show_main_menu(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # создание новых кнопок
+    btn1 = types.KeyboardButton('Профіль')
+    btn2 = types.KeyboardButton('Отримати звіт за дату')
+    btn3 = types.KeyboardButton('Підписатись на щоденний звіт')
+    btn4 = types.KeyboardButton('Написати до сервісу')
+    btn5 = types.KeyboardButton('Поповнити рахунок')
+
+    markup.add(btn1, btn2, btn3, btn4, btn5)
+    bot.send_message(message.from_user.id, 'Оберіть потрібну дію', reply_markup=markup)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Почати")
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "👋 Привіт! Я твій бот для роботы с відео спостереженням. \n"
+                                           "/start - почати с початку\n"
+                                           "Якщо ви тільки розпочали роботу с ботом то Вам необхідно мати 'Назву закладу' та 'Пароль'. Цю інформацію може отримати у власника закладу.\n "
+                                           "Наступний крок - підписатися не щоденний звіт"
+                                           "/menu - показати меню ", reply_markup=markup)
+
+@bot.message_handler(commands=['menu'])
+def start(message):
+
+    show_main_menu(message)
+
+@bot.message_handler(func=lambda message: message.reply_to_message is not None and
+                                          (message.reply_to_message.text == report_for_date or
+                                            message.reply_to_message.text ==  report_for_date_or_exit))
+
+@bot.message_handler(func=lambda message: message.reply_to_message is not None and
+                                          message.reply_to_message.text == est_name_and_password_for_subsc)
+
+@bot.message_handler(func=lambda message: message.reply_to_message is not None and
+                                          message.reply_to_message.text == top_up_account)
+
+def handle_reply(message):
+    if message.reply_to_message.text == report_for_date or message.reply_to_message.text == report_for_date_or_exit:
+        try:
+            date = datetime.strptime(message.text, '%Y-%m-%d')
+            bot.reply_to(message, f'Ви ввели вірний формат дати: {date}')
+            bot.reply_to(message, f'Йде перевірка')
+            bot.reply_to(message, f'Вы получите отчёт как только база данных будет готова')
+            show_main_menu(message)
+
+        except ValueError:
+            bot.reply_to(message, 'Невірний формат дати.')
+            markup = types.ForceReply(selective=False)
+            bot.send_message(message.from_user.id, report_for_date_or_exit, reply_markup=markup)
+
+    elif message.reply_to_message.text == est_name_and_password_for_subsc:
+        if(len(message.text.split(' ')) ==2):
+            name, passw = message.text.split(' ')
+            bot.reply_to(message, f'Ви ввели назву закладу: {name}\nПароль: {passw}')
+            db = DB()
+            res = db.subscribe_user_to_est(message.from_user.id, name, passw)
+            if (res == 'success'):
+                markup = types.ForceReply(selective=False)
+                bot.send_message(message.from_user.id, f'Ви успішно підписані на регулярний звіт. Очікуйте наступного звіту', reply_markup=markup)
+            else:
+                markup = types.ForceReply(selective=False)
+                bot.send_message(message.from_user.id, res, reply_markup=markup)
+
+            show_main_menu(message)
+        else:
+            bot.reply_to(message, f'Ви не ввели назву_закладу і пароль')
+            show_main_menu(message)
+    elif message.reply_to_message.text == top_up_account:
+        try:
+            val = int(message.text)
+            prices = [LabeledPrice(label='Product', amount=val * 100)]
+            bot.send_invoice(
+                chat_id=message.chat.id,
+                title="OKO AI",
+                description="Відео спостереження для вашого бізнесу",
+                provider_token=payment_token,  # Get this from your payment provider setup
+                currency="UAH",
+                is_flexible=False,  # True If you want to send a flexible invoice
+                prices=prices,
+                start_parameter="start_param",
+                invoice_payload="payload"
+            )
+        except ValueError:
+            bot.reply_to(message, 'Не правильний формат числа')
+            markup = types.ForceReply(selective=False)
+            bot.send_message(message.from_user.id, top_up_account, reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+
+    if message.text == '👋 Почати':
+        markup = types.ForceReply(selective=False)
+        bot.send_message(message.from_user.id, est_name_and_password_for_subsc, reply_markup=markup)
+
+    elif message.text == 'Профіль':
+        db = DB()
+        est_subs = db.get_est_list_for_tg_user(message.from_user.id)
+        messg = ''
+        if est_subs is None:
+            messg = 'Ви не підписані на жоден заклад\n'
+        else:
+            ests = ' '.join(est_subs)
+            messg = 'Ви підписані на заклади: ' + ests
+
+        est_subs = db.get_est_name_by_owner_id(message.from_user.id)
+
+        if est_subs is None:
+            messg += " \nВи не зареєстровані як власник закладу"
+        else:
+            ests = ' '.join(est_subs)
+            messg =  messg + '\nВи зареєсровані власником: ' + ests
+
+        messg = messg + f"\nУ вас на рахунку {db.get_money_for_tg_user(message.from_user.id)} UAH"
+
+        bot.send_message(message.from_user.id,
+                         messg,
+                         parse_mode='Markdown')
+
+
+
+    elif message.text == 'Отримати звіт за дату':
+        markup = types.ForceReply(selective=False)
+        bot.send_message(message.from_user.id, report_for_date, reply_markup=markup)
+
+    elif message.text == 'Підписатись на щоденний звіт':
+        markup = types.ForceReply(selective=False)
+        bot.send_message(message.from_user.id, est_name_and_password_for_subsc, reply_markup=markup)
+
+    elif message.text == 'Отримати інформацію про заклад':
+        markup = types.ForceReply(selective=False)
+        bot.send_message(message.from_user.id, "Введине название заведения и пароль",
+                         reply_markup=markup)
+
+    elif message.text == "Поповнити рахунок":
+        markup = types.ForceReply(selective=False)
+        db = DB()
+        #if db.is_tg_user_owner(message.from_user.id):
+        bot.send_message(message.from_user.id, top_up_account,
+                         reply_markup=markup)
+       # else:
+        #    bot.send_message(message.from_user.id,
+        #                     "Вы не являетесь владельцем заведения",
+        #                     parse_mode='Markdown')
+        show_main_menu(message)
+
+    elif message.text == "Проверить состояние счёта":
+        db = DB()
+        money = db.get_money_for_tg_user(message.from_user.id)
+        if money is None:
+            bot.send_message(message.from_user.id,
+                             "Данные не найдены",
+                             parse_mode='Markdown')
+        else:
+            bot.send_message(message.from_user.id,
+                             f"На счету {money} гривен",
+                             parse_mode='Markdown')
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except ConnectionError as e:
+            print("Connection error, retrying...", e)
+            time.sleep(15)
---
+++ dispatcher.py
@@ -0,0 +1,134 @@
+import os.path
+from multiprocessing.connection import Client
+from utils.general import LOGGER
+
+import schedule
+import time
+from utils.general import LOGGER
+from db import DB
+import re
+from bot import send_bot_message
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+days_untin_final = "У вас залишилося %s до завершення підписки\n"
+users_money = "У вас на рахунку %s\n"
+license_name_and_price = "Ви викостовуете для закладу %s підписку %s. Baм поповнити рахунок на %s грн"
+paying_for_license = "З Вашого рахунку списано %s за використання підписки %s для закладу %s. На на рахунку залишок %s"
+def get_date_from_file(source_path):
+    match = re.search(r'(\d{4}-\d{2}-\d{2})', source_path)
+    if match:
+        date_string = match.group(1)
+        converted_date = datetime.strptime(date_string, '%Y-%m-%d').date()
+        return converted_date
+    else:
+        return False
+
+def run_processing():
+
+    print(" Processing started")
+    db = DB()
+    db.cur.execute("SELECT * FROM establishments")
+    rows = db.cur.fetchall()
+    list_not_resp = []
+    for i in range(len(rows)):
+        id, name, adress, passw, license_id, owner_id, report_type, path, date = rows[i]
+        if path!= None and os.path.isdir(path):
+            for file in os.listdir(path):
+                if file.endswith('.mp4') and get_date_from_file(file):
+                        #and os.path.isfile(os.path.join(path, file)[:-3]+'json'):
+                    date_string = get_date_from_file(file)
+                    if date_string:
+                        days_difference = (datetime.now().date() - date_string).days
+                        if days_difference > 2:
+                            continue
+                        date = str(date_string)
+                        # Формируем запрос на выборку данных из таблицы по дате
+                        query = f"SELECT * FROM REPORT WHERE REALDATE = '{date}' AND ESTABLISHMENT_ID = {id}"
+                        # Выполняем запрос и получаем результат
+                        db.cur.execute(query)
+
+                        try:
+                            d = db.cur.fetchall()
+                            if len(d) > 0:
+                                continue
+
+                            while True:
+                                ip_server, id_server, device = db.get_server_for_task(list_not_resp)
+                                if len(ip_server) < 1:
+                                    return ''
+
+                                try:
+                                    address = (ip_server, 8443)
+                                    conn = Client(address)
+                                    if os.path.isdir(os.path.join(path,'frames')) is False:
+                                        try:
+                                            os.mkdir(os.path.join(path,'frames'))
+                                        except:
+                                            LOGGER.info(
+                                                str(datetime.now()[:-7]) + " Not connected to server " + ip_server)
+
+                                    command = f"--source={os.path.join(path, file)} --project={os.path.join(path,'frames')} --est={name} --id={id_server} --device={device}"
+                                    print(command)
+                                    conn.send(command)
+                                    conn.send('close')
+                                    conn.close()
+                                    break
+                                except:
+                                    LOGGER.info(str(datetime.now()[:-7]) + " Not connected to server " + ip_server)
+                                    list_not_resp.append(ip_server)
+                                    pass
+                                    continue
+
+                        except:
+                            pass
+
+    else:
+        return None
+def license_check():
+    db = DB()
+    rows = db.get_full_est_list()
+    #rows = db.cur.fetchall()
+    for i in range(len(rows)):
+        id, name, adress, passw, license_id, owner_id, report_type, path, date = rows[i]
+        days_difference = (date - datetime.now().date()).days
+        lic_name, lic_price = db.get_license_name_and_price(license_id)
+        tg_id = db.get_telegram_id(owner_id)
+        tg_user_money = db.get_money_for_tg_user(tg_id)
+        if ( days_difference < 7 and days_difference >= 0 and lic_name == "Test"):
+            result_string = days_untin_final % (days_difference)
+            if (tg_user_money == 0):
+                send_bot_message(result_string + " У вас на рахунку 0 грн. Тариф базовий буде призначений автоматично. Якщо треба змінити, тоді звертайтесь до оператора", tg_id)
+
+        elif ( days_difference < 7 and days_difference > 0 and lic_name !="Test"):
+            money = db.get_money_for_tg_user(tg_id)
+            if (money < lic_price):
+                result_string = days_untin_final % (days_difference)
+                result_string = result_string + users_money % (money) + license_name_and_price %(name, lic_name, lic_price - money)
+                send_bot_message(result_string, tg_id)
+
+        elif (days_difference <= 0 and lic_name != "Test"):
+            if (tg_user_money >= lic_price ):
+                db.addmomey_for_tg_user(tg_id, lic_price * -1)
+                result_string = paying_for_license %(lic_price, lic_name, name, db.get_money_for_tg_user(tg_id))
+                today = datetime.today()
+                one_month_later = today + relativedelta(months=1)
+                db.db_set_date_license_expired(one_month_later.date(), id)
+                send_bot_message(result_string, tg_id)
+
+
+# Планирование задачи на выполнение каждый день в определенное время
+schedule.every().day.at("03:00").do(run_processing)
+schedule.every().day.at("03:00").do(license_check)
+
+def main():
+    #license_check()
+    run_processing()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    #bot.polling(none_stop=True, interval=0)  # обязательная для работы бота часть
+    main()
